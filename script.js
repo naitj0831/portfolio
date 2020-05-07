1e7f0f351fb6bf82aced0aeaee7b830512a4af94
Naitdesign <60152936+naitj0831@users.noreply.github.com>
2020-05-06 23:05:49 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+(function(){
+    var words = [
+        ' graphic artist',
+        ' sculptor',
+        ' designer',
+        
+        ], i = 0;
+    setInterval(function(){
+        $('#changingword').fadeOut(function(){
+            $(this).html(words[i=(i+1)%words.length]).fadeIn();
+        });
+    }, 3000);
+      
+})();
+
